a88e76a7cb17ca8b2ee3f83661f1c7dd1155f9a2
Tomek Banach <tomek@banciur.org>
2018-11-09 01:58:31 +0100

    Basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: "Bike gears"
+    })
+  ],
+  devServer: {
+    contentBase: './dist'
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: "development"
+};
