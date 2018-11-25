74219a62e6bcb9262ab8113f3b8964c17750f7e7
Tomek Banach <tomek@banciur.org>
2018-11-26 00:16:13 +0100

    Remove redundant setting from webpack.config.js

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -13,7 +13,7 @@ module.exports = {
     }),
   ],
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json", ".css"],
+    extensions: [".ts", ".tsx", ".js", ".json"],
   },
   module: {
     rules: [
