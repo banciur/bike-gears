af79db934b646882c16617b4edd9aaad6089dc84
Tomek Banach <banciur@users.noreply.github.com>
2018-12-23 18:46:08 +0100

    Improve index templates (#11)

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,15 +1,9 @@
 const merge = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const configCommon = require("./webpack.config.common");
 
 module.exports = merge(configCommon, {
   mode: "development",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.template.dev",
-    }),
-  ],
   devServer: {
     contentBase: "./dist",
   },
