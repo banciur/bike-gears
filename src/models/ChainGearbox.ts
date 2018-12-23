111d41ca094a1084b5bf26ac11468e4281f7cdd8
Tomek Banach <banciur@users.noreply.github.com>
2018-12-23 23:11:46 +0100

    Primitive view for chain grarbox (#8)

--- src/models/ChainGearbox.ts
+++ src/models/ChainGearbox.ts
@@ -2,18 +2,26 @@ import { IGearbox } from "./IGearbox";
 import { Gearbox } from "./Gearbox";
 
 export class ChainGearbox implements IGearbox {
-  private readonly gears: [string, [number, number], number][] = [];
+  readonly gears: [string, [number, number], number][] = [];
   readonly length: number;
+  readonly frontGears: Gearbox;
+  readonly rearGears: Gearbox;
+  readonly forbiddenGears: string[];
 
   constructor(frontGears: number[], rearGears: number[], forbiddenGears: string[]) {
-    const front = new Gearbox(frontGears);
-    const rear = new Gearbox(rearGears, false);
+    this.frontGears = new Gearbox(frontGears);
+    this.rearGears = new Gearbox(rearGears, false);
+    this.forbiddenGears = forbiddenGears;
 
-    for (let f = 0; f < front.length; f++) {
-      for (let r = 0; r < rear.length; r++) {
+    for (let f = 0; f < this.frontGears.length; f++) {
+      for (let r = 0; r < this.rearGears.length; r++) {
         const gearIndex = `${f + 1}-${r + 1}`;
         if (!forbiddenGears.includes(gearIndex)) {
-          this.gears.push([gearIndex, [front.getRatio(f), rear.getRatio(r)], front.getRatio(f) / rear.getRatio(r)]);
+          this.gears.push([
+            gearIndex,
+            [this.frontGears.getRatio(f), this.rearGears.getRatio(r)],
+            this.frontGears.getRatio(f) / this.rearGears.getRatio(r),
+          ]);
         }
       }
     }
