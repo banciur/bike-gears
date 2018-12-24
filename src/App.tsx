880a1739d948434acda84c13985ed9c616926f9b
Tomek Banach <tomek@banciur.org>
2018-12-24 18:21:19 +0100

    Fix some names around ChainGearbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { Container, Row, Col } from "reactstrap";
 
-import { ChainGearBoxComponent } from "./components/ChainGearBox";
+import { ChainGearboxComponent } from "./components/ChainGearboxComponent";
 import { ChainGearbox } from "./models/ChainGearbox";
 
 const frontGears = [44, 32, 22];
 const rearGears = [11, 13, 15, 17, 19, 20, 22];
 const forbiddenGears = ["1-7", "1-6", "2-1", "2-7", "3-1", "3-2"];
 
-const gearbox = new ChainGearbox(frontGears, rearGears, forbiddenGears);
+const chainGearbox = new ChainGearbox(frontGears, rearGears, forbiddenGears);
 
 export const App = () => (
   <Container>
@@ -19,7 +19,7 @@ export const App = () => (
     </Row>
     <Row>
       <Col>
-        <ChainGearBoxComponent chainGearBox={gearbox} />
+        <ChainGearboxComponent chainGearbox={chainGearbox} />
       </Col>
     </Row>
   </Container>
