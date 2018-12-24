c19e32598dd1c3b387b33d8a8cf247570a57a1e7
Tomek Banach <tomek@banciur.org>
2018-12-24 21:40:15 +0100

    Add possibility to edit gears

--- src/components/ChainGearboxComponent.tsx
+++ src/components/ChainGearboxComponent.tsx
@@ -2,38 +2,77 @@ import * as React from "react";
 import { Row, Col } from "reactstrap";
 
 import { ChainGearbox } from "../models/ChainGearbox";
+import { Gearbox } from "../models/Gearbox";
+import { GearboxComponent } from "./GearboxComponent";
 
 export interface ChainGearboxProps {
   chainGearbox: ChainGearbox;
 }
 
-export const ChainGearboxComponent: React.FunctionComponent<ChainGearboxProps> = ({ chainGearbox }) => (
-  <Row>
-    <Col>
-      <h4>Front gears</h4>
-      {Array.from(chainGearbox.frontGears).map((ratio, index) => (
-        <div key={index}>{ratio}</div>
-      ))}
-    </Col>
-    <Col>
-      <h4>Rear gears</h4>
-      {Array.from(chainGearbox.rearGears).map((ratio, index) => (
-        <div key={index}>{ratio}</div>
-      ))}
-    </Col>
-    <Col>
-      <h4>Forbidden gears</h4>
-      {Array.from(chainGearbox.forbiddenGears).map((gear, index) => (
-        <div key={index}>{gear}</div>
-      ))}
-    </Col>
-    <Col>
-      <h4>Ratios</h4>
-      {Array.from(chainGearbox.gears).map(gear => (
-        <div key={gear[0]}>
-          {gear[0]} - {gear[2].toFixed(2)}
-        </div>
-      ))}
-    </Col>
-  </Row>
-);
+export interface ChainGearboxState {
+  chainGearbox: ChainGearbox;
+}
+
+export class ChainGearboxComponent extends React.Component<ChainGearboxProps, ChainGearboxState> {
+  constructor(props: ChainGearboxProps) {
+    super(props);
+    this.state = {
+      chainGearbox: props.chainGearbox,
+    };
+  }
+
+  frontGearsChangeHandler = (gearbox: Gearbox) => {
+    this.setState({
+      chainGearbox: new ChainGearbox(
+        Array.from(gearbox),
+        Array.from(this.state.chainGearbox.rearGears),
+        this.state.chainGearbox.forbiddenGears,
+      ),
+    });
+  };
+
+  rearGearsChangeHandler = (gearbox: Gearbox) => {
+    this.setState({
+      chainGearbox: new ChainGearbox(
+        Array.from(this.state.chainGearbox.frontGears),
+        Array.from(gearbox),
+        this.state.chainGearbox.forbiddenGears,
+      ),
+    });
+  };
+
+  render() {
+    return (
+      <Row>
+        <Col>
+          <h4>Front gears</h4>
+          <GearboxComponent
+            gearbox={this.state.chainGearbox.frontGears}
+            gearboxChangeHandler={this.frontGearsChangeHandler}
+          />
+        </Col>
+        <Col>
+          <h4>Rear gears</h4>
+          <GearboxComponent
+            gearbox={this.state.chainGearbox.rearGears}
+            gearboxChangeHandler={this.rearGearsChangeHandler}
+          />
+        </Col>
+        <Col>
+          <h4>Forbidden gears</h4>
+          {Array.from(this.state.chainGearbox.forbiddenGears).map((gear, index) => (
+            <div key={index}>{gear}</div>
+          ))}
+        </Col>
+        <Col>
+          <h4>Ratios</h4>
+          {Array.from(this.state.chainGearbox.gears).map(gear => (
+            <div key={gear[0]}>
+              {gear[0]} - {gear[2].toFixed(2)}
+            </div>
+          ))}
+        </Col>
+      </Row>
+    );
+  }
+}
---
+++ src/components/GearboxComponent.tsx
@@ -0,0 +1,93 @@
+import * as React from "react";
+import { Input } from "reactstrap";
+import { debounce } from "lodash";
+
+import { Gearbox } from "../models/Gearbox";
+
+const DEBOUNCE_WAIT = 300;
+
+interface GearboxComponentProps {
+  gearbox: Gearbox;
+  gearboxChangeHandler: (gearbox: Gearbox) => void;
+}
+
+interface GearboxComponentState {
+  gearbox: number[];
+}
+
+export class GearboxComponent extends React.Component<GearboxComponentProps, GearboxComponentState> {
+  private readonly notifyParent: (gearbox: Gearbox) => void;
+  private readonly ascending: boolean;
+
+  constructor(props: GearboxComponentProps) {
+    super(props);
+
+    this.notifyParent = props.gearboxChangeHandler;
+    this.ascending = props.gearbox.ascending;
+    this.state = {
+      gearbox: Array.from(props.gearbox),
+    };
+  }
+
+  recalculateAndNotify = (gearbox?: number[]) => {
+    const newGears = gearbox === undefined ? this.state.gearbox : gearbox;
+    const newGearbox = new Gearbox(newGears, this.ascending);
+    this.setState({
+      gearbox: Array.from(newGearbox),
+    });
+    this.notifyParent(newGearbox);
+  };
+
+  debouncedRecalculateAndNotify = debounce(this.recalculateAndNotify, DEBOUNCE_WAIT);
+
+  handleChangeGearGenerator = (index: number) => {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      const number = Number.parseInt(event.target.value, 10);
+      const newGears = Array.from(this.state.gearbox);
+      newGears[index] = number;
+      this.setState({
+        gearbox: newGears,
+      });
+      this.debouncedRecalculateAndNotify();
+    };
+  };
+
+  handleRemoveGearGenerator = (index: number) => {
+    return () => {
+      const newGears = Array.from(this.state.gearbox);
+      newGears.splice(index, 1);
+      this.setState({
+        gearbox: newGears,
+      });
+      this.debouncedRecalculateAndNotify();
+    };
+  };
+
+  debouncedNewGear = debounce((event: React.ChangeEvent<HTMLInputElement>) => {
+    const newGears = Array.from(this.state.gearbox);
+    newGears.push(Number.parseInt(event.target.value, 10));
+    event.target.value = "";
+    this.recalculateAndNotify(newGears);
+  }, DEBOUNCE_WAIT);
+
+  handleNewGear = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.persist();
+    this.debouncedNewGear(event);
+  };
+
+  render(): React.ReactNode {
+    return (
+      <>
+        {Array.from(this.state.gearbox).map((gear, index) => (
+          <div key={index}>
+            {index + 1} - <Input value={gear} onChange={this.handleChangeGearGenerator(index)} />{" "}
+            <span onClick={this.handleRemoveGearGenerator(index)}> X </span>
+          </div>
+        ))}
+        <div>
+          <Input onChange={this.handleNewGear} />
+        </div>
+      </>
+    );
+  }
+}
