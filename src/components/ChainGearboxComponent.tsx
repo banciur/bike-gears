aa79befe26401725729160713df3f0f2a994742e
Tomek Banach <tomek@banciur.org>
2018-12-24 18:21:19 +0100

    Fix some names around ChainGearbox

--- src/components/ChainGearboxComponent.tsx
+++ src/components/ChainGearboxComponent.tsx
@@ -3,33 +3,33 @@ import { Row, Col } from "reactstrap";
 
 import { ChainGearbox } from "../models/ChainGearbox";
 
-export interface ChainGearBoxProps {
-  chainGearBox: ChainGearbox;
+export interface ChainGearboxProps {
+  chainGearbox: ChainGearbox;
 }
 
-export const ChainGearBoxComponent: React.FunctionComponent<ChainGearBoxProps> = ({ chainGearBox }) => (
+export const ChainGearboxComponent: React.FunctionComponent<ChainGearboxProps> = ({ chainGearbox }) => (
   <Row>
     <Col>
       <h4>Front gears</h4>
-      {Array.from(chainGearBox.frontGears).map((ratio, index) => (
+      {Array.from(chainGearbox.frontGears).map((ratio, index) => (
         <div key={index}>{ratio}</div>
       ))}
     </Col>
     <Col>
       <h4>Rear gears</h4>
-      {Array.from(chainGearBox.rearGears).map((ratio, index) => (
+      {Array.from(chainGearbox.rearGears).map((ratio, index) => (
         <div key={index}>{ratio}</div>
       ))}
     </Col>
     <Col>
       <h4>Forbidden gears</h4>
-      {Array.from(chainGearBox.forbiddenGears).map((gear, index) => (
+      {Array.from(chainGearbox.forbiddenGears).map((gear, index) => (
         <div key={index}>{gear}</div>
       ))}
     </Col>
     <Col>
       <h4>Ratios</h4>
-      {Array.from(chainGearBox.gears).map(gear => (
+      {Array.from(chainGearbox.gears).map(gear => (
         <div key={gear[0]}>
           {gear[0]} - {gear[2].toFixed(2)}
         </div>
