a3b947d9e26b80bd16ce8ec924a40fda9633f597
Tomek Banach <tomek@banciur.org>
2018-12-30 19:46:59 +0100

    Move description out of React to make google crawler happy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Container, Row, Col } from "reactstrap";
 
 import { ChainGearboxComponent } from "./components/ChainGearboxComponent";
 import { ChainGearbox } from "./models/ChainGearbox";
@@ -10,21 +9,4 @@ const forbiddenGears = ["2-1", "3-2", "1-7", "1-6", "2-7", "3-1"];
 
 const chainGearbox = new ChainGearbox(frontGears, rearGears, forbiddenGears);
 
-export const App = () => (
-  <Container>
-    <Row>
-      <Col>
-        <h1>Another bike gears ratio calculator</h1>
-        <p>
-          So here you can enter your chainring sizes and calculate ratio for all gears. Yep at this time is as good as
-          excel but one of next feature is chart that will display cadence to speed.
-        </p>
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <ChainGearboxComponent chainGearbox={chainGearbox} />
-      </Col>
-    </Row>
-  </Container>
-);
+export const App = () => <ChainGearboxComponent chainGearbox={chainGearbox} />;
