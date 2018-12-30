31462e2d2f81e0c60b168524f0f5e7c3de884f05
Tomek Banach <tomek@banciur.org>
2018-12-30 19:39:23 +0100

    Add favicon and fix CleanWebpackPlugin usage

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -1,16 +1,18 @@
 const path = require("path");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
   devtool: "source-map",
   plugins: [
-    new CleanWebpackPlugin(["dist"]),
+    new CleanWebpackPlugin(["dist"], { root: path.resolve(__dirname, "..") }),
     new HtmlWebpackPlugin({
       template: "src/index.template",
       title: "Bike gears",
     }),
+    new CopyWebpackPlugin([{ from: "src/favicon", to: "favicon" }]),
   ],
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"],
