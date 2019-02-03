3bfc447e394db01e0450158c372e94375fbe9bee
Tomek Banach <tomek@banciur.org>
2019-02-03 14:50:50 +0100

    Add user preferences

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import * as React from "react";
 
 import { ChainGearboxComponent } from "./components/ChainGearboxComponent";
 import { ChainGearbox } from "./models/ChainGearbox";
+import { RiderPreferences } from "./models/RiderPreferences";
+import { RiderPreferencesComponent } from "./components/RiderPreferencesComponent";
+import { Container } from "reactstrap";
 
 const frontGears = [44, 32, 22];
 const rearGears = [11, 13, 15, 17, 19, 20, 22];
 const forbiddenGears = ["2-1", "3-2", "1-7", "1-6", "2-7", "3-1"];
 
 const chainGearbox = new ChainGearbox(frontGears, rearGears, forbiddenGears);
+const riderPreferences = new RiderPreferences(80, 120);
+const riderPreferencesChange = (newPrefs: RiderPreferences): void => {
+  console.log(newPrefs);
+};
 
-export const App = () => <ChainGearboxComponent chainGearbox={chainGearbox} />;
+export const App = () => (
+  <Container>
+    <RiderPreferencesComponent riderPreferences={riderPreferences} onChange={riderPreferencesChange} />
+    <ChainGearboxComponent chainGearbox={chainGearbox} />
+  </Container>
+);
